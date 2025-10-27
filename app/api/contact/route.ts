5a23d7a3d8a3ebc5592e12a8db949d0fa8790534
yabu1121 <n.hayato0122@outlook.jp>
2025-10-27 11:12:52 +0900

    taipo

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -5,19 +5,19 @@ export async function POST(request: Request) {
     const data = await request.json();
     const { name, email, content } = data;
     console.log(name, email, content);
-  
-  const transporter = nodemailer.createTransport({
-    service: "gamil",
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.GMAILUSER,
-      pass: process.env.GMAILPASSWORD
-    },
-  });
 
-  await transporter.sendMail({
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.GMAILUSER,
+        pass: process.env.GMAILPASSWORD
+      },
+    });
+
+    await transporter.sendMail({
       from: email,
       to: "hayabusa115346@gmail.com",
       subject: `[お問い合わせ] ${name} 様より`,
