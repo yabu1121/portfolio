b90641b1239f4f9099b9436778846c6c34b3e3d9
yabu1121 <n.hayato0122@outlook.jp>
2025-11-22 23:55:17 +0900

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
   ];
 
   return (
-    <header className="flex items-center justify-between w-full px-18 py-4 bg-white shadow-md">
+    <header className="flex w-full justify-between items-center px-6 md:px-12 py-4 bg-white shadow-md">
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold text-gray-800">hayato</h1>
       </div>
       <nav>
-        <ul className="flex gap-6">
+        <ul className="flex gap-4">
           {links.map((item) => {
               return(
               <li key={item.id} className="hover:text-blue-600">
