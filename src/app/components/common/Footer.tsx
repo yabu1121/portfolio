5b26c518a44457ab03d4d9e0607d0b6d055fd931
yabu1121 <n.hayato0122@outlook.jp>
2025-12-29 23:29:39 +0900

    spfooter complete

--- src/app/components/common/Footer.tsx
+++ src/app/components/common/Footer.tsx
@@ -1,38 +1,49 @@
 import { ActivityIcon, Highlighter, Rabbit, Send, UserRound } from "lucide-react"
+import Link from "next/link"
 
 const Footer = () => {
   return (
     <footer className='md:hidden bottom-0 fixed w-screen'>
       <ul className="bg-slate-300 flex justify-between w-full h-full">
         <li className="w-1/5 h-full flex items-center justify-center text-sm py-4">
-          <div>
-            <UserRound />
-            <p>Top</p>
-          </div>
+          <Link href="/">
+            <div>
+              <UserRound className="mx-auto" />
+              <p>Top</p>
+            </div>
+          </Link>
         </li>
         <li className="w-1/5 h-full flex items-center justify-center text-sm py-4">
-          <div>
-            <Highlighter />
-            <p>About</p>
-          </div>
+          <Link href="/about">
+            <div>
+              <Highlighter className="mx-auto" />
+              <p>About</p>
+            </div>
+          </Link>
         </li>
         <li className="w-1/5 h-full flex items-center justify-center text-sm py-4">
-          <div>
-            <ActivityIcon />
-            <p>Works</p>
-          </div>
+          <Link href="/projects">
+            <div>
+              <ActivityIcon className="mx-auto" />
+              <p>Works</p>
+            </div>
+          </Link>
         </li>
         <li className="w-1/5 h-full flex items-center justify-center text-sm py-4">
-          <div>
-            <Send />
-            <p>Contact</p>
-          </div>
+          <Link href="/contact">
+            <div>
+              <Send className="mx-auto" />
+              <p>Contact</p>
+            </div>
+          </Link>
         </li>
         <li className="w-1/5 h-full flex items-center justify-center text-sm py-4">
-          <div>
-            <Rabbit />
-            <p>Links</p>
-          </div>
+          <Link href="/links">
+            <div>
+              <Rabbit className="mx-auto" />
+              <p>Links</p>
+            </div>
+          </Link>
         </li>
       </ul>
     </footer>
