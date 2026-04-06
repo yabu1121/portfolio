410c8c3f6e93199a7e0481359bd3da51d6374ddf
yabu1121 <n.hayato0122@outlook.jp>
2026-04-06 11:43:23 +0900

    言語経験

--- src/app/components/AboutProfile.tsx
+++ src/app/components/AboutProfile.tsx
@@ -7,6 +7,11 @@ const AboutProfile = () => {
           <li>興味： バックエンドのパフォーマンス向上、DBの最適化などUXに直接かかわるようなプログラミング</li>
           <li>得意分野：Next.js TypeScript</li>
           <li>開発環境：Next.js TypeScript Golang</li>
+          <br />
+          <p>言語経験</p>
+          <li>TS: Next.js zustand zod toast</li>
+          <li>GO: colly gorm echo cobra-cli</li>
+          <li>python: fastAPI whisper OpenCV</li>
         </ul>
       </div>
     </section>
