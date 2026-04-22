e6ead9fef9a0be6bf67da0f0ad3816ca65f7224c
yabu1121 <n.hayato0122@outlook.jp>
2026-04-23 08:43:25 +0900

    width

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -1,13 +1,4 @@
-import { Tech, TimelineItem, Project, Links } from "@/app/utils/type"
-
-export const techs: Tech[] = [
-  { id: 1, name: "C", level:  30, description: "基礎、ファイル操作、ポインタ、ソートのアルゴリズム、二分木の探索"},
-  { id: 2, name: "C++", level: 30, description: "基礎部分、ソートのアルゴリズム、二分木の探索、DFS"},
-  { id: 3, name: "Python", level: 20, description: "Pandas, Numpyに少し触れた程度"},
-  { id: 4, name: "Go", level: 10, description: "基本構文を学習中"},
-  { id: 5, name: "Java", level: 20, description: "基礎、ファイル操作"},
-  { id: 6, name: "Next.js", level: 35, description: "Api操作、データベース接続学習中"},
-]
+import { Links } from "@/app/utils/type"
 
 export const links: Links[] = [
     { id: 1, topic: "Top", link: "/" },
