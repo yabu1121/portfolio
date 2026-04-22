23fe036662c57b08835f922387969f2ec43fd675
yabu1121 <n.hayato0122@outlook.jp>
2026-04-22 15:44:06 +0900

    front crud

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,16 +6,18 @@ import AdminAbout from "../components/AdminAbout";
 import AdminProject from "../components/AdminProject";
 import AdminContact from "../components/AdminContact";
 import AdminTech from "../components/AdminTech";
+import AdminEvent from "../components/AdminEvent";
 
 
 const Admin = () => {
-  const [tab, setTab] = useState<'about' | 'tech' | 'projects' | 'contact'>('about');
+  const [tab, setTab] = useState<'about' | 'tech' | 'event' | 'projects' | 'contact'>('about');
 
   const renderTab = () => {
     switch (tab) {
       case 'about': return <AdminAbout />
       case 'projects': return <AdminProject />
       case 'tech': return <AdminTech />
+      case 'event': return <AdminEvent />
       case 'contact': return <AdminContact />
     }
   }
@@ -27,6 +29,7 @@ const Admin = () => {
         <TabButton title="about" label="プロフィール" tab={tab} setTab={setTab}/>
         <TabButton title="projects" label="プロジェクト" tab={tab} setTab={setTab}/>
         <TabButton title="tech" label="技術" tab={tab} setTab={setTab}/>
+        <TabButton title="event" label="参加履歴" tab={tab} setTab={setTab}/>
         <TabButton title="contact" label="メールアドレス" tab={tab} setTab={setTab}/>
       </div>
       {renderTab()}
--- src/app/components/AboutEvent.tsx
+++ src/app/components/AboutEvent.tsx
@@ -1,19 +1,19 @@
+'use client'
+
+import { api } from "@/trpc/client"
+
 const AboutEvent = () => {
- return (
+  const {data} = api.event.getAll.useQuery()
+  return (
     <section className="px-5 sm:px-8 py-6 sm:py-8 bg-white my-4 rounded">
       <div className="mb-10">
         <h2 className="text-2xl mb-2 font-medium">Event</h2>
         <p className="text-xs">参加してきたイベントなど</p>
         <div className="mt-4 space-y-2 px-3 py-3 text-sm sm:text-base leading-relaxed">
           <ul>
-            <li>2024年10月技育camp</li>
-            <li>2025年10月技育祭秋</li>
-            <li>2025年12月技育camp</li>
-            <li>2026年2月サポーターズ関東ユーザー会</li>
-            <li>2026年3月サポーターズ1on1面談</li>
-            <li>2026年3月go junction</li>
-            <li>2026年3月findyハッカソン</li>
-            <li>2026年3月技育祭春</li>
+            {data?.map((item) => (
+              <li key={item.id}>{item.year}年{item.month}月{item.name}</li>
+            ))}
           </ul>
         </div>
       </div>
