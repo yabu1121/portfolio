73dd02024ff340a6e7cea280feba9218092242ae
yabu1121 <n.hayato0122@outlook.jp>
2026-04-24 17:31:52 +0900

    crud

--- src/server/routers/skills.ts
+++ src/server/routers/skills.ts
@@ -1,13 +1,14 @@
 import { myTechs, techs } from "@/db/schema";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { adminProcedure, createTRPCRouter, publicProcedure } from "../trpc";
 import { eq } from "drizzle-orm";
+import z, { number, string } from "zod";
 
 export const skillRouter = createTRPCRouter({
   getAll: publicProcedure.query(async({ctx}) => {
-    const {db} = ctx;
+    const { db } = ctx;
     const skillData = await db
     .select({
-      id: techs.id,
+      id: myTechs.id,
       name: techs.name,
       iconUrl: techs.iconUrl,
       level: myTechs.level,
@@ -16,5 +17,74 @@ export const skillRouter = createTRPCRouter({
     .from(techs)
     .innerJoin(myTechs, eq(myTechs.techId, techs.id))
     return skillData;
+  }),
+
+  getByID: adminProcedure
+  .input(z.object({
+    id: string().uuid(),
+  }))
+  .query(async ({ctx, input}) => {
+    const { db } = ctx;
+    const { id } = input;
+    const [skill] = await db
+    .select({
+      id: myTechs.id,
+      name: techs.name,
+      iconUrl: techs.iconUrl,
+      level: myTechs.level,
+      description: techs.description,
+    })
+    .from(techs)
+    .innerJoin(myTechs, eq(myTechs.techId, techs.id))
+    .where(eq(myTechs.id, id))
+    .limit(1)
+    return skill;
+  }),
+
+  create: adminProcedure
+  .input(z.object({
+    techId: string().uuid(),
+    level: number().int().min(1).max(100),
+    description: string(),
+  }))
+  .mutation(async({ctx, input}) => {
+    const { db } = ctx;
+    const [inserted] = await db
+    .insert(myTechs)
+    .values(input)
+    .returning()
+    return inserted
+  }),
+
+  delete: adminProcedure
+  .input(z.object({
+    id: string().uuid()
+  }))
+  .mutation(async ({ctx, input}) => {
+    const { db } = ctx;
+    const { id } = input;
+    const [deleted] = await db
+    .delete(myTechs)
+    .where(eq(myTechs.id, id))
+    .returning()
+    return deleted
+  }),
+
+  update: adminProcedure
+  .input(z.object({
+    id: string().uuid(),
+    techId: string().uuid(),
+    level: number().int().min(1).max(100),
+    description: string(),
+  }))
+  .mutation(async ({ctx, input}) => {
+    const { db } = ctx;
+    const { id, ...values } = input;
+    const [updated] = await db
+    .update(myTechs)
+    .set(values)
+    .where(eq(myTechs.id, id))
+    .returning()
+    return updated
   })
 })
