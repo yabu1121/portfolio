496f07607f0116d40e99f93edf1591ba88b51189
yabu1121 <n.hayato0122@outlook.jp>
2026-08-13 20:04:42 +0900

    Allow GitHub image thumbnails

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: '',
+        pathname: '/**',
+      },
     ]
   },
   allowedDevOrigins: [
